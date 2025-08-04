569a122619fa2ccf38309972aa10f9d8bbec6036
Nick-ui911 <nbaghel392@gmail.com>
2025-08-04 14:15:56 +0530

    second

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import os from "os";
 export const app = express();
 
 app.get("/", (req, res) => {
-    // Simulate heavy computation
-    for (let i = 0; i < 1e9; i++) {
-        Math.random();
-    }
-    res.send("Hello World");
+  res.send("Hello World");
+});
+
+app.get("/cpu", (req, res) => {
+  // Simulate heavy computation
+  for (let i = 0; i < 10000000000; i++) {
+    Math.random();
+  }
+  res.send("Hello World");
 });
 
 app.get("/host", (req, res) => {
-    res.send(`Hostname: ${os.hostname()}`);
+  res.send(`Hostname: ${os.hostname()}`);
 });
